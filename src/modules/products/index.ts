fcbf052bff8fc9e2d49a1d471848ae60c0a30025
retr0lbb <retr0lbb@gmail.com>
2025-08-12 13:44:19 -0300

    feat: add title to schema

--- src/modules/products/index.ts
+++ src/modules/products/index.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from "fastify";
 import { listProductsRoute } from "./routes/list-products.route";
+import { createProductRoute } from "./routes/create-product.route";
 
 export const productsModule = async (app: FastifyInstance) => {
 	app.register(listProductsRoute);
+	app.register(createProductRoute);
 };
